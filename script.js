2b4917174622bae386512cca44b11a3e8887ba7b
srilekhachakinala <145462855+srilekhachakinala@users.noreply.github.com>
2024-06-28 12:58:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let menu= document.querySelector('#menu-icon');
+let navbar= document.querySelector('.navbar');
+
+menu.onclick = () => {
+
+    menu.classList.toggle('bx-x');
+
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+
+    menu.classList.remove('bx-x');
+
+    navbar.classList.remove('active');
+}
+
+
+const typed= new Typed('.multiple-text',{
+
+    strings: ['WeightLifting', 'Fat loss', 'Running', 'Weight Gain'],
+
+    typeSpeed: 60,
+
+    backSpeed: 60,
+
+    backDelay: 1000,
+
+    loop: true,
+
+});
